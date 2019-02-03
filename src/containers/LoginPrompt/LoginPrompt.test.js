bc19d348425c969f5a54b3477126a96132445748
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-02 20:17:07 -0700

    Test mapDispatchToProps in LoginPrompt

--- src/containers/LoginPrompt/LoginPrompt.test.js
+++ src/containers/LoginPrompt/LoginPrompt.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import LoginPrompt from './LoginPrompt.js';
+import { LoginPrompt, mapDispatchToProps } from './LoginPrompt.js';
+import { toggleLoginPrompt } from '../../actions';
 
 describe('LoginPrompt', () => {
   let wrapper;
@@ -13,6 +14,16 @@ describe('LoginPrompt', () => {
   describe('LoginPrompt component', () => {
     it('should properly render the component elements', () => {
       expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with the action toggleLoginPrompt', () => {
+      const expectedAction = toggleLoginPrompt(true);
+      const dispatchMock = jest.fn();
+      const props = mapDispatchToProps(dispatchMock);
+      props.toggleLoginPrompt(true);
+      expect(dispatchMock).toHaveBeenCalledWith(expectedAction);
     });
   });
 });
