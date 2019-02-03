23ed6ad1b244d22bf043b1892f97f810741fe268
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-02 20:17:43 -0700

    Remove unnecessary imports in MovieCard test file

--- src/containers/MovieCard/MovieCard.test.js
+++ src/containers/MovieCard/MovieCard.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { MovieCard, mapStateToProps, mapDispatchToProps }  from './MovieCard';
-import { setFavorites, toggleLoginPrompt } from '../../actions';
+import { toggleLoginPrompt } from '../../actions';
 
 const mockProps = { 
   title: 'frozen',
