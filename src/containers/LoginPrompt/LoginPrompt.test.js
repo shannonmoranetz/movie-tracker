da2bd22ac19d18c4dc20cbdee6885cd7d34176db
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-05 13:07:36 -0700

    Add tests to increase test coverage

--- src/containers/LoginPrompt/LoginPrompt.test.js
+++ src/containers/LoginPrompt/LoginPrompt.test.js
@@ -5,15 +5,31 @@ import { toggleLoginPrompt } from '../../actions';
 
 describe('LoginPrompt', () => {
   let wrapper;
+  const toggleLoginPromptMock = jest.fn();
   beforeEach(() => {
     wrapper = shallow(
-      <LoginPrompt />
+      <LoginPrompt toggleLoginPrompt={toggleLoginPromptMock} />
     );
   });
 
   describe('LoginPrompt component', () => {
     it('should properly render the component elements', () => {
       expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should call toggleLoginPrompt with false when the close button is clicked', () => {
+      wrapper.find('.close-button').simulate('click');
+      expect(toggleLoginPromptMock).toHaveBeenCalledWith(false);
+    });
+    
+    it('should call toggleLoginPrompt with false when the sign up button is clicked', () => {
+      wrapper.find('.signupprompt-button').simulate('click');
+      expect(toggleLoginPromptMock).toHaveBeenCalledWith(false);
+    });
+    
+    it('should call toggleLoginPrompt with false when the login button is clicked', () => {
+      wrapper.find('.loginprompt-button').simulate('click');
+      expect(toggleLoginPromptMock).toHaveBeenCalledWith(false);
     });
   });
 
